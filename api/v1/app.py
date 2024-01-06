326dec20caedc21f6e34b43264aab34afe65eef0
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-06 18:49:23 +0300

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,8 +2,9 @@
 """Your first endpoint (route) will be to return the status of your API"""
 
 from flask import Flask
-from models import storage
+from ..models import storage
 from api.v1.views import app_views
+import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
@@ -12,3 +13,9 @@ app.register_blueprint(app_views)
 def teardown_context():
     """cleanup"""
     storage.close()
+
+
+if __name__ == '__main__':
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True, debug=True)
