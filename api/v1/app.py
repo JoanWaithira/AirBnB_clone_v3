b3db4d840f05fcb7ed064f2bb10a4587e46eeaa8
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-07 10:51:55 +0300

    handled 4040

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Your first endpoint (route) will be to return the status of your API"""
 
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -13,6 +13,14 @@ app.register_blueprint(app_views, url_prefix='/api/v1')
 def teardown_context(exception=None):
     """cleanup"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(Exception=None):
+    payload = {"error": "Not found"}
+    response = make_response(jsonify(payload), 404)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 
 if __name__ == '__main__':
