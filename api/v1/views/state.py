3eb6a6dc5335facb550becce5d4625cf4643cbd7
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-07 17:37:07 +0300

    states route

--- api/v1/views/state.py
+++ api/v1/views/state.py
@@ -2,13 +2,13 @@
 """
 A view for State objects
 """
-from api.v1.views import state_view
+from api.v1.views import app_views
 from models import storage
 from models.state import State
-from flask import make_response, jsonify
+from flask import make_response, jsonify, abort
 
 
-@state_view.route("/api/v1/states", methods=['GET'], strict_slashes=False)
+@app_views.route("/states", methods=['GET'], strict_slashes=False)
 def states():
     """retrieves a list of state objects"""
     payload = {}
@@ -17,3 +17,13 @@ def states():
     response = make_response(jsonify(payload), 200)
     response.headers['Content-Type'] = 'application/json'
     return response
+
+
+@app_views.route("/states/<state_id>", methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """retrieves state by id"""
+    obj = storage.get(State, str(state_id))
+    if obj:
+        return jsonify(obj.to_dict())
+    else:
+        abort(404)
