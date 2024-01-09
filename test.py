0554f729927320085158c197fbbd79eace070ea7
philipnyakwaka1 <philipnyakwaka24@gmail.com>
2024-01-09 12:57:19 +0300

    updated

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-class Test:
-    def __init__(self, name):
-        self.name = name
+from models import storage
+from models.state import State
 
-
-review_obj = Test('Philip')
-new_dict = {'age': 25, 'id': 4}
-for key, val in new_dict.items():
-    if key not in ['created_at', 'updated_at', 'user_id', 'id']:
-        setattr(review_obj, key, val)
-print(review_obj.__dict__)
+print(storage.get(State, 'dec5b901-7ead-4417-a229-d523e7505b34').name)
